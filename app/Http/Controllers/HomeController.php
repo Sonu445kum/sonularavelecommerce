0d73285952c851c76343dbeca34a8fa00b5eb85d
Sonu445Kum <sonuroy1629@gmail.com>
2026-02-16 21:03:02 +0530

    add images url for sliding on the home pages

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,17 +14,17 @@ class HomeController extends Controller
         // ✅ Correct image paths (using /storage/images instead of /images)
         $sliderImages = [
             [
-                'url' => asset('storage/images/baaner3.jpg'),
+                'url' => 'https://images.unsplash.com/photo-1481437156560-3205f6a55735?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2hvcHBpbmd8ZW58MHx8MHx8fDA%3D',
                 'caption' => 'Upgrade Your Lifestyle',
                 'subtext' => 'Discover premium collections tailored just for you.',
             ],
             [
-                'url' => asset('storage/images/banner2.jpg'),
+                'url' => "https://images.unsplash.com/photo-1582004531564-50f300aae039?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGVjb21tZXJjZXxlbnwwfHwwfHx8MA%3D%3D",
                 'caption' => 'Exclusive Offers Await You',
                 'subtext' => 'Grab the best deals of the season before they’re gone!',
             ],
             [
-                'url' => asset('storage/images/banner1.jpg'),
+                'url' => "https://images.unsplash.com/photo-1555529669-e69e7aa0ba9a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c2hvcHBpbmd8ZW58MHx8MHx8fDA%3D",
                 'caption' => 'Shop Smarter, Live Better',
                 'subtext' => 'Quality products, fast delivery, and secure checkout.',
             ],
